463d21dbcc26e7a09e1fb0989f5c7441f3d9c828
Mkhululi Cebani <mkhululicebani01@gmail.com>
2021-07-14 14:00:22 +0200

    burgernav v2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import Fade from 'react-reveal'
+// import Fade from 'react-reveal'
 import {useSelector, useDispatch} from 'react-redux'
 
 import MenuIcon from '@material-ui/icons/Menu';
